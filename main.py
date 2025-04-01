26cd6d2607a75a202c95cd0960394c84efdfa5a9
Ignacio Gómez-Centurión Golmayo <igomezcenturiong@student.universidadunie.com>
2025-04-01 09:42:56 +0200

    examen

---
+++ main.py
@@ -0,0 +1,5 @@
+from lanzador import Lanzador  # Importa la función principal del módulo lanzador
+
+class main:
+    def __init__(self):
+        Lanzador()  # Llama al constructor de la clase Lanzador
